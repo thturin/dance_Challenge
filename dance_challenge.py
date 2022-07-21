0f7d2d89d95cf5c7b4a872c835e6eddbadb457d9
Tatiana Turin <tatiana.turin@gmail.com>
2022-07-20 22:29:30 -0400

    Initial commit

---
+++ dance_challenge.py
@@ -0,0 +1,201 @@
+import pgzrun
+from random import randint
+"""
+In this game, the dancer performs a sequence of moves and you need to repeat them using your arrow keys 
+generate a sequence of dance moves 
+create a countdown
+display moves on the screen
+check if user pressed the correct buttons 
+if correct, continue. if not, end game
+"""
+
+
+#CONSTANTS
+WIDTH = 800
+HEIGHT = 600
+CENTER_X = WIDTH/2
+CENTER_Y = HEIGHT/2
+#global vars
+move_list = [] #contain the dance moves
+display_list = []
+
+score = 0
+current_move = 0
+count = 4
+dance_length = 4
+
+#flags
+say_dance = False
+show_countdown = True
+moves_complete = False
+game_over = False
+
+#add the actors
+dancer = Actor('dancer-start')
+dancer.pos = CENTER_X+5,CENTER_Y-40
+
+up = Actor("up")
+up.pos = CENTER_X, CENTER_Y + 110
+
+right = Actor("right")
+right.pos = CENTER_X + 60, CENTER_Y + 170
+
+down = Actor("down")
+down.pos = CENTER_X, CENTER_Y + 230
+
+left = Actor("left")
+left.pos = CENTER_X - 60, CENTER_Y + 170
+
+def draw():
+    global game_over, sdcore, say_dance
+    global count, show_countdown
+
+    if not game_over:
+        screen.clear()
+        screen.blit('stage',(0,0))
+
+        dancer.draw()
+        up.draw()
+        down.draw()
+        right.draw()
+        left.draw()
+
+        screen.draw.text('Score: {}'.format(score),color='black',topleft=(10,10))
+
+        if say_dance:
+            screen.draw.text('Dance!', color='black',topleft=(CENTER_X-65,150),fontsize=60)
+        if show_countdown:
+            screen.draw.text('{}'.format(count), color='black',topleft=(CENTER_X-8,150),fontsize=60)
+    else:
+        screen.clear()
+        screen.blit('stage',(0,0))
+        screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
+        screen.draw.text("GAME OVER!", color="black",topleft=(CENTER_X - 130, 220), fontsize=60)
+    return
+
+def reset_dancer():
+    global game_over
+    if not game_over:
+        dancer.image = 'dancer-start'
+        up.image = 'up'
+        right.image = 'right'
+        left.image = 'left'
+        down.image = 'down'
+    return
+
+def update_dancer(move): #will update the image of the dancer to match the move paramter and highlight which arrow to press
+    global game_over
+    if not game_over:
+        if move == 0: #if move is 'up'
+            up.image = 'up-lit'
+            dancer.image = 'dancer-up'
+            clock.schedule(reset_dancer,0.5) #reset dancer back to normal
+        if move == 1: #if move is 'right'
+            right.image = 'right-lit'
+            dancer.image = 'dancer-right'
+            clock.schedule(reset_dancer, 0.5)  # reset dancer back to normal
+        if move == 2: #if move is 'right'
+            down.image = 'down-lit'
+            dancer.image = 'dancer-down'
+            clock.schedule(reset_dancer, 0.5)  # reset dancer back to normal
+        if move == 3: #if move is 'right'
+            left.image = 'left-lit'
+            dancer.image = 'dancer-left'
+            clock.schedule(reset_dancer, 0.5)  # reset dancer back to normal
+    return
+
+def display_moves():
+    global display_list, say_dance, show_countdown
+    if display_list: #if there are moves in the display list
+        this_move = display_list[0] #current move is the first item in the list
+        display_list = display_list[1:] # removes the first first item from display list
+        #or
+        #display_list = display_list.pop(0)
+        if this_move == 0:
+            update_dancer(0)
+            clock.schedule(display_moves,1) #will display the next move 1 second RECURSIVE FUNCTION
+        if this_move == 1:
+            update_dancer(1)
+            clock.schedule(display_moves, 1)
+        if this_move == 2:
+            update_dancer(2)
+            clock.schedule(display_moves, 1)
+        if this_move == 3:
+            update_dancer(3)
+            clock.schedule(display_moves, 1)
+    else:
+        say_dance = True #tells draw() to change
+        show_countdown = False
+    return
+
+def generate_moves():
+    global move_list, display_list, show_countdown, count,say_dance
+    rand_num = 0
+    count = 4
+    move_list = []
+    say_dance = False
+    for index in range(dance_length):
+        rand_num = randint(0,3)
+        move_list.append(rand_num)
+        display_list.append(rand_num)
+    show_countdown = True
+    countdown()
+    print('move_list={}'.format(move_list))
+    return
+
+def countdown():
+    global count, show_countdown
+    if count > 1:
+        count-=1
+        clock.schedule(countdown,1)
+    else:
+        show_countdown = False
+        display_moves()
+    return
+
+def next_move():
+    global current_move, moves_complete
+    if current_move < dance_length-1:
+        current_move += 1
+    else:
+        moves_complete = True
+    return
+
+def on_key_up(key):
+    global score,game_over,move_list,current_move
+    if key == keys.UP:
+        update_dancer(0)
+        if move_list[current_move] == 0:
+            score +=1
+            next_move()
+    if key == keys.RIGHT:
+        update_dancer(1)
+        if move_list[current_move] == 1:
+            score +=1
+            next_move()
+    if key == keys.DOWN:
+        update_dancer(2)
+        if move_list[current_move] == 2:
+            score +=1
+            next_move()
+    if key == keys.LEFT:
+        update_dancer(3)
+        if move_list[current_move] == 3:
+            score +=1
+            next_move()
+    return
+
+def update():
+    global current_move, moves_complete
+    if not game_over:
+        if moves_complete:
+            generate_moves()
+            moves_complete = False
+            current_move = 0
+    else:
+        music.stop()
+
+
+generate_moves()
+music.play('vanishing-horizon')
+pgzrun.go()
